b679f071f0e29704e2279bc285b02c73862029c0
guime7000 <tguimezanes@gmail.com>
2022-10-28 21:28:36 +0200

    Filling repo

---
+++ Secret/secret_json_creator.py
@@ -0,0 +1,17 @@
+import json
+import os
+
+"""
+Creates the JSON file for very ultra basic authentification...
+"""
+
+secretDirectoryPath = '/somewhere_over_the_rainbow/Secret/'
+
+dico = {"user1" : "pwd1",
+        "user2" : "pwd2"
+        }
+
+filePath = os.path.join(secretDirectoryPath,'secret.JSON')
+
+with open(filePath,'w') as outFIle :
+    json.dump(dico, outFIle, indent=2)
---
+++ Tools/archiveFiles_initial_creator.py
@@ -0,0 +1,39 @@
+import json
+import time
+import os
+
+"""
+Creates the initial JSON archive file for each roboat and fleet
+"""
+
+boatsDirectoryPath = '/somewhere_on_your_computer/Boats/'
+
+
+fleetList = ["groziboat",
+            "axelair",
+            "shapeshifters",
+            "modiababord",
+            "millesabords",
+            "chatboatez",
+            "echeneis",
+            "totorson",
+            "deepsink",
+            "robotak",
+            "fleet"]
+
+dico = {"ts" : 0,
+            "lat" : 0,
+            "lon" : 0,
+            "distanceToEnd" : 0,
+            "rank" : 0,
+            "speed" : 0,
+            "tws" : 0,
+            "twa" : 0,
+            "heading": 0
+    }
+
+for inName in fleetList :
+   filePath = os.path.join(boatsDirectoryPath,inName,inName + '_Arch.JSON')
+
+   with open(filePath,'w+') as outFIle :
+       json.dump([dico], outFIle, indent=2)
